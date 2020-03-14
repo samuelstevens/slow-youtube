f6b64072d5ef786fc0199be775894b8a06757348
Sam Stevens <stevens.994@osu.edu>
2020-03-14 11:43:34 -0400

    updated regex for the more common use case

--- src/Util.ts
+++ src/Util.ts
@@ -2,8 +2,8 @@ const parseChannelId = (url: string): string | null => {
   // takes https://www.youtube.com/channel/UCMu3_s7WCRCJrbh37UKmJ-A/featured?flow=grid
   // returns UCMu3_s7WCRCJrbh37UKmJ-A
 
-  // FUTURE: must have a trailing / after the channel id. Could that cause problems?
-  const re = /http?s:\/\/(?:www\.)?youtube.com\/channel\/(.*?)\/.*/;
+  // FUTURE: must NOT have a trailing / after the channel id. Could that cause problems?
+  const re = /http?s:\/\/(?:www\.)?youtube.com\/channel\/(.*)/;
 
   const match = url.match(re);
 
